77191d0a4d458f65db7cea8e5835dbf33ed3f2a8
Andrei <andreiflorianbratila@gmail.com>
2026-04-22 18:59:11 +0200

    cambios vista cache sin seguridad

--- src/app/(admin)/dashboard/vista/page.tsx
+++ src/app/(admin)/dashboard/vista/page.tsx
@@ -1,9 +1,11 @@
+import { cacheLife } from "next/cache";
 import Link from "next/link";
 
 export default async function Page() {
   console.log("se ejecuta page/dashboard/vista");
   return (
     <section className="rounded-2xl border border-zinc-200 bg-zinc-50 p-5 text-sm text-zinc-700">
+      <CachedComponent />
       <p>
         Esta subruta tambien exige verificacion server-side real y depende del
         mismo guard que <code>/dashboard</code>.
@@ -15,4 +17,10 @@ export default async function Page() {
       </p>
     </section>
   );
+}
+async function CachedComponent() {
+  "use cache: remote";
+  cacheLife("max");
+  console.log("me renderizo CachedComponent en vista");
+  return <h1>ESTO esta cacheado</h1>;
 }
